13557f911d82a8d2f2cccf88c4bddaf3523dcc5e
ravi19999 <dahalravindra@gmail.com>
2020-08-01 01:20:43 +0545

    k

--- server.py
+++ server.py
@@ -44,5 +44,9 @@ def submit():
     else:
         return 'something went wrong'
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port = port)d
+
 
 
